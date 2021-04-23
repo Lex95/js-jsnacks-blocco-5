389318ad8c6bc43646c62451c9a03f33601482f0
Lex95 <andrea.lega1995@gmail.com>
2021-04-23 15:28:03 +0200

    risoluzione secondo esercizio

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -1,22 +1,22 @@
 var zucchine = [
     {
         varieta: "bella",
-        peso:  100,
+        peso: 100,
         lunghezza: 15
     },
     {
         varieta: "brutta",
-        peso:  120,
+        peso: 120,
         lunghezza: 16
     },
     {
         varieta: "bella",
-        peso:  120,
+        peso: 120,
         lunghezza: 10
     },
     {
         varieta: "bella",
-        peso:  110,
+        peso: 110,
         lunghezza: 20
     },
     {
@@ -26,12 +26,12 @@ var zucchine = [
     },
     {
         varieta: "brutta",
-        peso:  130,
+        peso: 130,
         lunghezza: 18
     },
     {
         varieta: "storta",
-        peso:  100,
+        peso: 100,
         lunghezza: 15
     },
     {
@@ -46,7 +46,34 @@ var zucchine = [
     },
     {
         varieta: "tonda",
-        peso:  100,
+        peso: 100,
         lunghezza: 14
     }
 ];
+
+var SOGLIA = 15;
+var short = [];
+var long = [];
+
+for (var i = 0; i < zucchine.length; i++) {
+    if (zucchine[i].lunghezza < SOGLIA) {
+        short.push(zucchine[i]);
+    } else {
+        long.push(zucchine[i]);
+    }
+}
+
+function calcTot(arr, chiave) {
+    var totale = 0;
+    for (var i = 0; i < arr.length; i++) {
+        totale += arr[i][chiave];
+    };
+    return totale
+}
+
+var pesoCorte = calcTot(short, "peso");
+var pesoLunghe = calcTot(long, "peso");
+
+
+document.getElementById("myH1").innerHTML = "Il peso totale delle " + short.length + " zucchine corte è " + pesoCorte + " grammi.";
+document.getElementById("myH1").innerHTML += "<br>Il peso totale delle " + long.length + " zucchine lunghe è " + pesoLunghe + " grammi.";
