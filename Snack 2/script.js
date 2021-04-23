f3404a4f13e2999c2321825be8bce4b1c0b3e9ff
Lex95 <andrea.lega1995@gmail.com>
2021-04-23 15:14:18 +0200

    inizializzazione secondo esercizio

---
+++ Snack 2/script.js
@@ -0,0 +1,52 @@
+var zucchine = [
+    {
+        varieta: "bella",
+        peso:  100,
+        lunghezza: 15
+    },
+    {
+        varieta: "brutta",
+        peso:  120,
+        lunghezza: 16
+    },
+    {
+        varieta: "bella",
+        peso:  120,
+        lunghezza: 10
+    },
+    {
+        varieta: "bella",
+        peso:  110,
+        lunghezza: 20
+    },
+    {
+        varieta: "storta",
+        peso: 80,
+        lunghezza: 17
+    },
+    {
+        varieta: "brutta",
+        peso:  130,
+        lunghezza: 18
+    },
+    {
+        varieta: "storta",
+        peso:  100,
+        lunghezza: 15
+    },
+    {
+        varieta: "bella",
+        peso: 90,
+        lunghezza: 12
+    },
+    {
+        varieta: "tonda",
+        peso: 95,
+        lunghezza: 13
+    },
+    {
+        varieta: "tonda",
+        peso:  100,
+        lunghezza: 14
+    }
+];
